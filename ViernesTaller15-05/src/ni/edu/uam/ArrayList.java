e42fd056f1169f599bcb546af01344649d618e0e
Your Name <jrromero9855@uamv.edu.ni>
2026-05-15 16:34:08 -0600

    Primer Array List

--- ViernesTaller15-05/src/ni/edu/uam/ArrayList.java
+++ ViernesTaller15-05/src/ni/edu/uam/ArrayList.java
@@ -1,4 +1,21 @@
 package ni.edu.uam;
 
-public class ArrayList {
+import java.util.ArrayList;
+
+class MiArrayList {
+    public static void main(String[] args) {
+        ArrayList<Integer> numeros = new ArrayList<>();
+
+        numeros.add(10);
+        numeros.add(25);
+        numeros.add(30);
+        numeros.add(45);
+        numeros.add(60);
+
+        System.out.println("Elementos del ArrayList:");
+
+        for (int numero : numeros) {
+            System.out.println(numero);
+        }
+    }
 }
